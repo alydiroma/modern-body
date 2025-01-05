f9dfe3170fa7ccac3e147c134343b216c5d14ca7
ahanshaw <angela.hanshaw@gmail.com>
2025-01-05 16:44:56 -0600

    homepage progress

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,39 +1,55 @@
-// home banner video
-const hero = document.querySelector(".home-hero");
-const heroVideo = document.querySelector(".home-hero-video");
-const heroBtn = document.querySelector(".home-hero-play");
+import getHeight from './modules/getHeight';
 
-heroBtn.addEventListener("click", handlePlayButton, false);
-playVideo();
+// classes toggles
+function setItemsHeight() {
+	const classItemsList = document.querySelector('.home-classes-items'),
+	classItems = document.querySelectorAll('.home-classes-item');
+	
+	let classesHeight = 0;
 
-async function playVideo() {
-	try {
-		await heroVideo.play();
-		heroBtn.setAttribute('title', 'Pause Video');
-		heroBtn.classList.add('playing');
-		hero.classList.add('in-view');
-	} catch (err) {
-		heroBtn.setAttribute('title', 'Play Video');
-		heroBtn.classList.remove('playing');
-		hero.classList.add('no-autoplay');
-	}
+	classItems.forEach((item) => {
+		setTimeout(() => {
+			const itemHeight = item.dataset.height;
+			if (itemHeight > classesHeight) {
+				classesHeight = itemHeight;
+				classItemsList.style.height = itemHeight + 'px';
+			}
+		}, 100);
+	});
 }
 
-function handlePlayButton() {
-	if (heroVideo.paused) {
-		playVideo();
-		heroBtn.setAttribute('title', 'Pause Video');
-		heroBtn.classList.add('playing');
-		console.log('video was paused');
-	} else {
-		heroVideo.pause();
-		heroBtn.setAttribute('title', 'Play Video');
-		heroBtn.classList.remove('playing');
-		console.log('video was playing');
-	}
+const classes = () => {
+	const classToggles = document.querySelectorAll('.home-classes-toggle'),
+	classItems = document.querySelectorAll('.home-classes-item');
+
+	setItemsHeight();
+	classToggles.forEach((toggle) => {
+		toggle.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			classToggles.forEach((toggleItem) => {
+				toggleItem.setAttribute('aria-expanded', false);
+				toggleItem.classList.remove('active');
+			});
+
+			const toggleBtn = e.target,
+				toggleId = toggleBtn.id;
+
+			toggleBtn.setAttribute('aria-expanded', true);
+			toggleBtn.classList.add('active');
+
+			// set items
+			classItems.forEach((item) => {
+				if (item.getAttribute('aria-labelledby') === toggleId) {
+					item.classList.add('active');
+				}
+				else {
+					item.classList.remove('active');
+				}
+			});
+		});
+	});
 }
 
-heroVideo.addEventListener("ended", function() {
-	heroBtn.setAttribute('title', 'Play Video');
-	heroBtn.classList.remove("playing");
-}, false);
+classes();
+window.addEventListener("resize", setItemsHeight);
